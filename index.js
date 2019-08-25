62b55dc93a1f04ad6ab88298dcdcedb5fbaf3b8a
perochorichard <IAmTheDeveloperNow@gmail.com>
2019-08-25 15:45:27 -0400

    added background image and navbar

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
+    $("#Home").click(function() {
+        // TODO: jump 
+    });
     $("#about").click(function() {
-        
+        // TODO: smoothscroll to information secion of page
     });
 });
