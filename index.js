a040e7f0891f910b410b966e63a02eab55d5221b
perochorichard <IAmTheDeveloperNow@gmail.com>
2019-08-28 03:51:06 -0400

    added scroll detection script

--- index.js
+++ index.js
@@ -6,9 +6,15 @@ $(document).ready(function() {
         // TODO: smoothscroll to information secion of page
     });
 
-    var scrollCounter = $("#scroll-counter");
-    $(window).scroll(function() {
-        var value = $(this).scrollTop();
-        scrollCounter.html(value);
+    var $scrollCounter = $("#scroll-counter");
+    var $window = $(window);
+    $window.scroll(function() {
+        var screenPercent = 1 - ($window.scrollTop() / $window.height());
+        $scrollCounter.html(screenPercent);
+        if (screenPercent <= 0.5) {
+            $("#bg").css({filter: blur(8)});
+        } else {
+            $("#bg").css({filter: blur(0)});
+        }
     });
 });
