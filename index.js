e26070fd7ce9e87aba3517bd0c3725bcf270239f
perochorichard <IAmTheDeveloperNow@gmail.com>
2019-08-28 03:19:28 -0400

    fixed horizontal overflow

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ $(document).ready(function() {
     $("#about").click(function() {
         // TODO: smoothscroll to information secion of page
     });
+
+    var scrollCounter = $("#scroll-counter");
+    $(window).scroll(function() {
+        var value = $(this).scrollTop();
+        scrollCounter.html(value);
+    });
 });
