a36984f320d8f3b6d363c6ad0274e5e1fc35d693
perochorichard <IAmTheDeveloperNow@gmail.com>
2019-09-03 22:26:08 -0400

    fixed background image blur white edges

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-$(document).ready(function() {
-    $("#Home").click(function() {
-        // TODO: jump 
-    });
-    $("#about").click(function() {
-        // TODO: smoothscroll to information secion of page
-    });
-
+$(document).ready(function () {
     var $scrollCounter = $("#scroll-counter");
     var $window = $(window);
     var $bg = $("#bg");
-    $window.scroll(function() {
+
+    $window.scroll(function () {
         var screenPos = ($window.scrollTop() / $window.height());
         $scrollCounter.html(screenPos);
-        var blur = "blur(" + (screenPos) * 8 + "px)";
+        blur = "blur(" + (screenPos) * 8 + "px)";
         $bg.css("filter", blur);
     });
 });
